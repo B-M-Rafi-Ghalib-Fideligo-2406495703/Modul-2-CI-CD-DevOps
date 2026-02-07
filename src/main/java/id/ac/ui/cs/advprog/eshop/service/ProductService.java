fa6b1c12bc3bd669a4221e17f8063279937ed745
Fideligo <mrafighalibf@gmail.com>
2026-02-07 17:50:01 +0700

    feat: Implement functional Delete feature with consistent URL pattern

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -8,4 +8,5 @@ public interface ProductService {
     public List<Product> findAll();
     public Product getProductById(String productId);
     public Product update(Product product);
+    public void delete(String productId);
 }
